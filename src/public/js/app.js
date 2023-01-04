fefcced56c40709d89d91d09d939c8cea99485c2
JCW <codnjs8221@naver.com>
2023-01-05 00:48:10 +0900

    socket 연결 실습 코드 구현 (미완성. 소켓 연결 에러가 뜸)

---
+++ src/public/js/app.js
@@ -0,0 +1,3 @@
+
+// 서버와 연결된 socket
+const socket = new WebSocket(`ws://localhost:3000`);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
-console.log("hello");
+// 환경 설정
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+// static 설정
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(3000);
+// route 설정
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+// http 서버 생성
+const server = http.createServer(app);
+// http 서버 위에 ws 서버 생성
+const wss = new WebSocket.Server({ server });
+
+// websocket이 연결됐을 때 실행되는 callback func
+// socket : 브라우저와 서버 간의 연결
+function handleConnection(socket) {
+  console.log(socket);  
+}
+// websocket event 등록
+wss.on("connection", handleConnection);
+
+app.listen(3000, handleListen);
