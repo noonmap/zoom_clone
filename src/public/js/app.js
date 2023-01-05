67e74416692995e5d99858a041e83801ad240da5
JCW <codnjs8221@naver.com>
2023-01-06 00:21:52 +0900

    1-7 : 메세지 type으로 메세지 구분 (JSON, 직렬화)
    - nickname과 message를 구분하기 위해, {type, payload}를 포함하는 JSON을 전송함
    - socket.send는 string만 보낼 수 있기 때문에 JSON을 직렬화함

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`); // 서버와 연결된 socket
 
-// 서버와 연결된 socket
-const socket = new WebSocket(`ws://${window.location.host}`);
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 // socket이 connection을 open했을 때 이벤트 추가
 socket.addEventListener("open", () => {
@@ -11,7 +15,10 @@ socket.addEventListener("open", () => {
 
 // socket에 message 받는 이벤트 추가
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data, " from the server");
+    // 받은 메세지를 화면에 표시
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 // 서버와의 connection 종료 이벤트 추가
@@ -19,18 +26,17 @@ socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
 });
 
-// 10초 후 동작
-setTimeout(() => {
-    socket.send("[after 10s] hello from the browser!");
-}, 10000);
-
 function handleSubmit(event) {
     event.preventDefault();
-    // pug에서 입력한 form 메세지 값을 가져옴 -> submit
-    const input = messageForm.querySelector("input");
-    // input 메세지를 서버로 전송
-    socket.send(input.value);
-    // input 메세지를 비워줌
-    input.value = "";
+    const input = messageForm.querySelector("input"); // pug에서 입력한 form 메세지 값을 가져옴 -> submit
+    socket.send(input.value); // input 메세지를 서버로 전송
+    input.value = ""; // input 메세지를 비워줌
 }
 messageForm.addEventListener("submit", handleSubmit);
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value)); // JSON object를 직렬화해서 전송
+}
+nickForm.addEventListener("submit", handleNickSubmit);
