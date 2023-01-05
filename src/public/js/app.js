b659576b196e12e6a6b7c2f3ddf35cec1b6b458c
JCW <codnjs8221@naver.com>
2023-01-05 17:51:27 +0900

    1-4 진행중

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,15 @@
 // 서버와 연결된 socket
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socket이 connection을 open했을 때 이벤트 추가
+socket.addEventListener("open", () => {
+    console.log("Connected to Server 👍");
+});
+
+// socket에 message 받는 이벤트 추가
+socket.addEventListener("message", (message) => {
+    console.log("Just go this: ", message, " from the server");
+});
+
+// 서버와의 connection 종료 이벤트 추가
+socket.addEventListener("close", () => {});
--- src/server.js
+++ src/server.js
@@ -16,17 +16,20 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-// http 서버 생성
+// express app으로부터 http 서버 생성
 const server = http.createServer(app);
 // http 서버 위에 ws 서버 생성
 const wss = new WebSocket.Server({ server });
 
-// websocket이 연결됐을 때 실행되는 callback func
-// socket : 브라우저와 서버 간의 연결
-function handleConnection(socket) {
-    console.log(socket);
-}
 // websocket event 등록
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) {
+  // websocket이 연결됐을 때 실행되는 callback func
+  // socket : 브라우저와 서버 간의 연결
+    console.log("Connected to Browser 👍");
+  // wss 서버가 아니라 socket에 있는 메소드 사용
+  // data 전송
+  socket.send("hello world!!");
+});
 
+// http 서버에 접근 access
 server.listen(3000, handleListen);
