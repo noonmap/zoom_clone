7a837e45950b439c116ebbe4241429809844417a
JCW <codnjs8221@naver.com>
2023-01-07 14:52:01 +0900

    3-1. 음소거, 카메라 on/off 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
 let myStream; // stream = video + audio
+let muted = false;
+let cameraOff = false;
 
 async function getMedia() {
     try {
@@ -21,3 +25,30 @@ async function getMedia() {
 }
 
 getMedia();
+
+function handleMuteClick() {
+    // Audio track을 mute/unmute
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+    // 버튼 토글
+    if (muted) {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    } else {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    }
+}
+function handleCameraClick() {
+    // Video track을 on/off
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    // 버튼 토글
+    if (cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
